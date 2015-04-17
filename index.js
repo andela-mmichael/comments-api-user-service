03821fdc13113eb804277ca844471e50928f2397
mel <mel.michael@andela.co>
2015-04-17 12:39:28 +0100

    renamed file

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+var express = require('express');
+var app = express();
 var usermodel = require('./app/users/models/user.model');
+var config = require('./config/db.config');
+
+app.listen(config.port, function(){
+  console.log('App working at port: ' + config.port);
+});
