3f28d07c6048bcaadaa4f4f119daded6ac8a2e09
mel <mel.michael@andela.co>
2015-04-20 14:59:55 +0100

    Impement test for API end points

--- tests/user/user.route.spec.js
+++ tests/user/user.route.spec.js
@@ -17,51 +17,54 @@ var testUser;
 
 describe('End to End Test for User Routes', function(){
 
-  xdescribe('Route Test for /signup', function(){
+  xdescribe('Route Test for POST /signup - ', function(){
     beforeEach(function(done){
       testUser = User.forge({
         username: 'user11',
         email: 'user11@user.com',
         password: 1111
-        // auth: 'testoken'
       });
       done();
     });
 
     //check that username is provided
-    it('should not create a new user without a username', function(done){
+    it('should not create new user without a username', function(done){
       testUser.set({username: null});
       request(app)
         .post('/signup')
         .send(testUser)
         .expect(400)
         .end(function(err, res){
-          if(err) return done(err);
-          expect(res.body.data).toEqual(jasmine.objectContaining({
-            data: 'Incorrect Information'
+          if(err) {
+            return done(err);
+          }
+          expect(res.body).toEqual(jasmine.objectContaining({
+            message: 'Username is required'
           }));
           done();
         });
     });
 
     //check that password is provided
-    it('should not create a new user without a password', function(done){
+    it('should not create new user without a password', function(done){
       testUser.set({password: null});
       request(app)
         .post('/signup')
         .send(testUser)
         .expect(400)
         .end(function(err, res){
-          if(err) return done(err);
-          expect(res.body.data).toEqual(jasmine.objectContaining({
-            data: 'Wrong Information'
+          if(err) {
+            return done(err);
+          }
+          expect(res.body).toEqual(jasmine.objectContaining({
+            message: 'Password is required'
           }));
           done();
         });
     });
 
     //check that email is provided
-    it('should not create a new user without an email', function(done){
+    it('should not create new user without an email', function(done){
       testUser.set({email: null});
       request(app)
         .post('/signup')
@@ -71,11 +74,25 @@ describe('End to End Test for User Routes', function(){
           if(err) {
             return done(err);
           }
-          expect(res.body.data).toEqual(jasmine.objectContaining({
-            data: 'Wrong Information'
+          expect(res.body).toEqual(jasmine.objectContaining({
+            message: 'Email is required'
           }));
           done();
         });
+    });
+
+    //check that username is unique
+    it('should not create new user if user already exist', function(done){
+      testUser.save().then(function(){});
+      request(app)
+        .post('/signup')
+        .send({username: 'user11', email: 'user11@user.com', password: 1111})
+        .expect(409)
+        .end(function(err){
+          expect(err).not.toBeDefined();
+          done();
+        });
+      
     });
 
     //check that a new user is created
@@ -88,20 +105,32 @@ describe('End to End Test for User Routes', function(){
           if(err){
             return done(err);
           }
-          expect(res.body).toBeDefined();
+          expect(res.body).toEqual(jasmine.objectContaining({
+            username: 'user11',
+            email: 'user11@user.com',
+            password: 1111
+          }));
+          done();
+        });
+    });
+
+    afterEach(function(done){
+      knex('users')
+        .where('username', 'user11')
+        .del()
+        .then(function(){
           done();
         });
     });
   });
 
-  describe('Route Test for /login', function(){
+  xdescribe('Route Test for POST /login - ', function(){
     beforeEach(function(done){
       testUser = User.forge(
       {
         username: 'user22',
         email: 'user22@user.com',
-        password: '2222',
-        auth: 'usertoken'
+        password: '2222'
       });
       done();
     });
@@ -110,7 +139,7 @@ describe('End to End Test for User Routes', function(){
     it('should not authenticate if no username is provided', function(done){
       testUser.set({username: null});
       request(app)
-        .post('/signup')
+        .post('/login')
         .send(testUser)
         .expect(401)
         .end(function(err, res){
@@ -126,7 +155,7 @@ describe('End to End Test for User Routes', function(){
     it('should not authenticate if no password is provided', function(done){
       testUser.set({password: null});
       request(app)
-        .post('/signup')
+        .post('/login')
         .send(testUser)
         .expect(401)
         .end(function(err, res){
@@ -138,11 +167,76 @@ describe('End to End Test for User Routes', function(){
       });
     });
 
+    //check that user data is returned with a token
+    it('should authenticate and return user data and a token', function(done){
+      request(app)
+        .post('/login')
+        .send(testUser)
+        .expect(200)
+        .end(function(err, res){
+          expect(res.body).toEqual(jasmine.objectContaining({
+            username: 'user22',
+            email: 'user22@user.com',
+            password: '2222',
+            auth: 'testoken'
+          }));
+          done();
+        });
+    });
+
     afterEach(function(done){
       knex('users')
         .where('username', 'user22')
         .del()
         .then(function(){
+          done();
+        });
+    });
+  });
+
+  describe('GET /users', function(){
+    beforeEach(function(done){
+      Users
+        .forge([{
+          username: 'user33',
+          email: 'user33@user.com',
+          password: '3333',
+          last_name: 'damon',
+          first_name: 'dallas',
+          auth: 'token33'
+        },
+        {
+          username: 'user44',
+          email: 'user44@user.com',
+          password: '4444',
+          last_name: 'stefan',
+          first_name: 'dallas',
+          auth: 'token44'
+        }])
+        .invokeThen('save')
+        .then(function(users){
+          done();
+        });
+    });
+
+    it('should throw an exception if the user is not authenticated', function(done){
+      request(app)
+        .get('/users')
+        .expect(401)
+        .end(function(err, res){
+          expect(response.body).toEqual(jasmine.objectContaining({
+            message: 'Unauthorized!!!'
+          }));
+          done();
+        });
+    });
+
+    afterEach(function (done) {
+      knex('users')
+        .where('username', 'user33')
+        .orWhere('username', 'user44')
+        .del()
+        .then(function() {
           done();
         });
     });
