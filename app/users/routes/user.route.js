0a4298d70555fff19db79fa4f10b5007c05f285e
mel <mel.michael@andela.co>
2015-04-20 15:32:29 +0100

    Create route for end points

--- app/users/routes/user.route.js
+++ app/users/routes/user.route.js
@@ -1,1 +1,16 @@
-user.route.js
+var Controller = require('../controllers/user.controller');
+var express = require('express');
+var Router = express.Router();
+
+Router
+  .get('/', Controller.getAllUsers)
+
+  .post('/', Controller.createUser)
+
+  .get('/:username', Controller.getAUser)
+
+  .put('/:username', Controller.updateUser)
+
+  .delete('/:username', Controller.removeUser);
+
+module.exports = Router;
