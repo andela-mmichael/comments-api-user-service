67ebb39a79acffd728929746d0a058273c352ac4
mel <mel.michael@andela.co>
2015-04-21 14:27:20 +0100

    Modified files for logic implementation

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 var usermodel = require('./app/users/models/user.model');
 var express = require('express');
+var Router = require('./app/users/routes/user.route');
 var app = express();
 
 //configure body parser
 var bodyParser = require('body-parser');
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 //connect to database
 var config = require('./config/db.config');
 config.db[process.env.NODE_ENV];
 
 //route middleware
-// app.use('/user', router)
+app.use('/', Router)
 
 app.listen(config.port, function(){
   console.log('App working at port: ' + config.port);
 });
 
-model.exports = app;
+module.exports = app;
